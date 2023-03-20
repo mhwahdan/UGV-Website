1681400dbda3190e9faec84319eb4e3c110c08c7
mhwahdan <moustafawahdan55@gmail.com>
2023-03-21 00:28:27 +0200

    added Identity

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { footerComponent } from './footer/footer.component';
